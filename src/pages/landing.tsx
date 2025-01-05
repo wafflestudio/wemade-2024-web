7487bc614063e3a4dd1cb3b02464bcfc05a14592
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2025-01-05 21:17:53 +0900

    feat:랜딩페이지 만들기

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
+import { Layout } from '@/components/Layout';
+import LoginButton from '@/components/LoginButton';
+import { BLACK_LOGO } from '@/constants/images';
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Layout>
+      <div className="flex h-full flex-col items-center justify-center">
+        <div className="mb-[82px] flex flex-col items-center justify-center">
+          <img
+            src={BLACK_LOGO}
+            alt="logo"
+            className="w-[274.8px]"
+          />
+          <div className="font-isans text-xl text-titleBlack">
+            안녕하세요 위메이드입니다.
+          </div>
+        </div>
+        <LoginButton text="Google 계정으로 로그인하기" />
+        <div className="my-[38px] flex w-[320px] items-center">
+          <hr className="flex-grow border-t border-textGrey1" />
+          <span className="mx-3.5 text-sm text-textGrey1">OR</span>
+          <hr className="flex-grow border-t border-textGrey1" />
+        </div>
+        <div className="text-[15px] text-textGrey2">
+          처음이시라면?
+          <a
+            href="/"
+            className="ml-2.5 font-psemibold text-textGreen underline"
+          >
+            회원가입하기
+          </a>
+        </div>
+      </div>
+    </Layout>
+  );
 };
 
 export default Landing;
