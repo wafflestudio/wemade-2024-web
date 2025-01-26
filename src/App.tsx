a2c9e909e326b99ac926797cdc0b2ac4bf643392
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2025-01-26 16:11:35 +0900

    ProtectedRoute 로  로그인 유저 route 구분

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import { ProtectedRoute } from '@/components/Auth';
 import { Home } from '@/pages/home';
 import { Landing } from '@/pages/landing';
 import { Signup } from '@/pages/signup';
 
 import '@/styles/styles.css';
 const routes = [
-  { path: '/', element: <Landing /> },
-  { path: '/signup', element: <Signup /> },
-  { path: '/home', element: <Home /> },
+  {
+    path: '/',
+    element: <ProtectedRoute redirectTo="/auth/login" />,
+    children: [{ path: '', element: <Signup /> }],
+  },
+  { path: '/auth/login', element: <Landing /> },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+  {
+    path: '/signup',
+    element: <ProtectedRoute redirectTo="/auth/login" />,
+    children: [{ path: '', element: <Signup /> }],
+  },
 ];
 
 const router = createBrowserRouter(routes);
