23e28cdc4f3501ddddfe885bcc7b769c87353328
Jaewoong Choi <jwchoi.tech@gmail.com>
2025-02-03 01:30:33 +0900

    feat: Icon 및 관련 로직 추가

---
+++ src/constants/icons.tsx
@@ -0,0 +1,19 @@
+import ADD_ICON from '@/assets/icons/add-icon.svg?react';
+import BACKWARD_ICON from '@/assets/icons/backward-icon.svg?react';
+import CROP_ICON from '@/assets/icons/crop-icon.svg?react';
+import DELETE_ICON from '@/assets/icons/delete-icon.svg?react';
+import EDIT_ICON from '@/assets/icons/edit-icon.svg?react';
+import HISTORY_ICON from '@/assets/icons/history-icon.svg?react';
+import LIST_ICON from '@/assets/icons/list-icon.svg?react';
+import TREE_ICON from '@/assets/icons/tree-icon.svg?react';
+
+export const Icons = {
+  Add: <ADD_ICON />,
+  Backward: <BACKWARD_ICON />,
+  Crop: <CROP_ICON />,
+  Delete: <DELETE_ICON />,
+  Edit: <EDIT_ICON />,
+  History: <HISTORY_ICON />,
+  List: <LIST_ICON />,
+  Tree: <TREE_ICON />,
+};
