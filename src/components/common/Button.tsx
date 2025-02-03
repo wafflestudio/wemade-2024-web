909e4fc48ce92518742dcaff68646535a9a87ec4
Jaewoong Choi <jwchoi.tech@gmail.com>
2025-02-04 03:14:03 +0900

    feat: Button 컴포넌트에 스타일 추가 및 다른 Button들 삭제

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,44 +1,53 @@
+import clsx from 'clsx';
 import React from 'react';
 
 type ButtonProps = {
-  theme?: 'gray' | 'green' | 'red';
-  size?: 'small' | 'large';
+  label: string;
+  theme?: 'gray' | 'green' | 'red' | 'white';
+  size?: 'small' | 'medium' | 'large';
   className?: string;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button = ({
-  children,
-  theme = 'gray', // 기본값 gray
-  size = 'small', // 기본값 small
+  label,
+  theme = 'gray',
+  size = 'small',
   className = '',
   disabled,
   ...props
 }: ButtonProps) => {
-  const baseStyles = 'transition-all font-pmedium';
+  const baseStyles = 'transition-all';
   const disabledStyles = 'bg-backgroundGray text-textGray1';
 
   const buttonTheme = {
-    gray: 'bg-textGray1 text-backgroundWhite hover:bg-textGray2',
-    green: 'bg-pointColor text-titleBlack hover:bg-backgroundGreen',
-    red: 'bg-[#E7564C] text-backgroundWhite hover:bg-[#D04338]',
+    gray: 'bg-textGray1 text-white hover:bg-textGray2',
+    green: 'bg-pointColor text-black hover:bg-backgroundGreen',
+    red: 'bg-[#E7564C] text-white hover:bg-[#D04338]',
+    white:
+      'bg-white text-black hover:bg-backgroundGray border border-borderGray',
   };
 
   const sizeStyles = {
     small:
-      'h-[21px] px-[6px] py-[4px] text-[11px] rounded-[30px] leading-[13.13px]',
+      'h-[21px] px-[6px] py-[4px] text-[11px] rounded-[30px] leading-[13.13px] font-medium',
+    medium:
+      'h-[34px] px-[16px] py-[8px] text-[15px] rounded-[6px] leading-[13.13px] font-semibold',
     large:
-      'h-[34px] px-[16px] py-[8px] text-[15px] rounded-[6px] leading-[13.13px]',
+      'h-[36px] w-full px-[16px] py-[8px] text-[15px] rounded-[6px] leading-[17.9px] font-semibold',
   };
 
   return (
     <button
-      className={`${baseStyles} ${sizeStyles[size]} ${
-        disabled ? disabledStyles : buttonTheme[theme]
-      } ${className}`}
+      className={clsx(
+        baseStyles,
+        sizeStyles[size],
+        disabled ? disabledStyles : buttonTheme[theme],
+        className
+      )}
       disabled={disabled}
       {...props}
     >
-      {children}
+      {label}
     </button>
   );
 };
