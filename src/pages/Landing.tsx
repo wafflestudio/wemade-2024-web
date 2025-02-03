3f755f8d7398553feba6c0132b621c916f338d03
Jaewoong Choi <jwchoi.tech@gmail.com>
2025-02-04 03:09:48 +0900

    refactor: landing, signup 페이지 UI에 맞게 리팩터링

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,53 +1,45 @@
-import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-import SearchBar from '@/components/common/SearchBar.tsx';
-import { Layout } from '@/components/Layout';
-import LoginButton from '@/components/LoginButton';
+import Button from '@/components/common/Button.tsx';
 import { BLACK_LOGO } from '@/constants/images.ts';
 
 const Landing = () => {
-  const [isClicked, setIsClicked] = useState(false);
-
   const handleLogin = async () => {
-    setIsClicked(!isClicked);
     window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=${import.meta.env.VITE_DOMAIN}/home&prompt=consent&response_type=code&client_id=${import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID}&scope=openid%20email%20profile&access_type=offline`;
   };
 
   return (
-    <Layout>
-      <div className="flex h-full flex-col items-center justify-center">
-        <div className="mb-[82px] flex flex-col items-center justify-center">
-          <img
-            src={BLACK_LOGO}
-            alt="logo"
-            className="w-[274.8px]"
-          />
-          <div className="font-isans text-xl text-titleBlack">
-            안녕하세요 위메이드입니다.
-          </div>
-        </div>
-        <LoginButton
-          text="Google 계정으로 로그인하기"
-          onClick={handleLogin}
-          isClicked={isClicked}
-        />
-        <div className="my-[38px] flex w-[320px] items-center">
-          <hr className="flex-grow border-t border-textGray1" />
-          <span className="mx-3.5 text-sm text-textGray1">OR</span>
-          <hr className="flex-grow border-t border-textGray1" />
-        </div>
-        <div className="text-[15px] text-textGray2">
-          처음이시라면?
-          <Link
-            to="/signup"
-            className="ml-2.5 font-psemibold text-textGreen underline"
-          >
-            회원가입하기
-          </Link>
-        </div>
+    <div className="flex h-full w-[320px] flex-col items-center justify-center">
+      <img
+        src={BLACK_LOGO}
+        alt="logo"
+        className="mb-[18px] w-[274.8px]"
+      />
+      <p className="mb-[82px] text-[25.2px] font-black text-black">
+        안녕하세요 위메이드입니다.
+      </p>
+      <Button
+        label="Google 계정으로 로그인하기"
+        theme="white"
+        size="large"
+        onClick={handleLogin}
+        className="!font-medium"
+      />
+      <div className="my-[38px] flex w-[320px] items-center">
+        <hr className="flex-grow border-t border-textGray1" />
+        <span className="mx-3.5 text-sm text-textGray1">OR</span>
+        <hr className="flex-grow border-t border-textGray1" />
       </div>
-    </Layout>
+      <div className="text-[15px]">
+        <span className="text-textGray2">처음이시라면?</span>
+        <Link
+          to="/signup"
+          className="ml-2.5 font-semibold text-textGreen underline"
+        >
+          회원가입하기
+        </Link>
+      </div>
+    </div>
   );
 };
 
