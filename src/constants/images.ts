b6bba305cfcbac5de20b2a150581d4d8a2827059
Jaewoong Choi <jwchoi.tech@gmail.com>
2025-02-04 03:12:32 +0900

    refactor: 불필요한 로고 이미지 정리

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,6 +1,4 @@
-import BACK_BUTTON from '@/assets/icons/backward-icon.svg';
-import SIGNUP_LOGO from '@/assets/images/signup.svg';
-import BLACK_LOGO from '@/assets/images/wemade-logo-black.png';
-import GRAY_SMALL_LOGO from '@/assets/images/wemade-logo-gray-small.svg';
+import BLACK_LOGO from '@/assets/images/wemade-logo-black.svg';
+import GRAY_LOGO from '@/assets/images/wemade-logo-gray.svg';
 
-export { BLACK_LOGO, GRAY_SMALL_LOGO, BACK_BUTTON, SIGNUP_LOGO };
+export { BLACK_LOGO, GRAY_LOGO };
