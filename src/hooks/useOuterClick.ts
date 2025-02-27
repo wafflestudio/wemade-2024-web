77803379f172195b5077ab688fd2d0e63a4ab045
조유진 <uzinfamily@snu.ac.kr>
2025-02-28 05:50:58 +0900

    feat: dropdown component

---
+++ src/hooks/useOuterClick.ts
@@ -0,0 +1,30 @@
+import { RefObject, useEffect } from 'react';
+
+const useOuterClick = (
+  ref: RefObject<HTMLElement>,
+  callback: () => void,
+  ignoreRefs?: RefObject<HTMLElement>[]
+) => {
+  const handleClick = (e: MouseEvent) => {
+    if (ref.current && !ref.current.contains(e.target as Node)) {
+      if (
+        ignoreRefs &&
+        ignoreRefs.some((ignoreRef) =>
+          ignoreRef.current?.contains(e.target as Node)
+        )
+      ) {
+        return;
+      }
+      callback();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClick);
+    return () => {
+      document.removeEventListener('mousedown', handleClick);
+    };
+  }, [ref, callback]);
+};
+
+export default useOuterClick;
